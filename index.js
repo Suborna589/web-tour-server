afbd333eb89e94c19b98728d2e489ed6abb036bd
Suborna589 <chakrobortis101@gmail.com>
2024-08-26 01:39:15 +0600

    'third'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
-const cors= require('cors');  
+const cors= require('cors'); 
+const jwt =require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlrxmsc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,14 +31,131 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // Connect the client to the server	(optional starting in v4.7) 
+   await client.connect();
+
+
+    const userCollection = client.db("tourismDb").collection("users");
+    const spotCollection = client.db("tourismDb").collection("tourist");
+    const cartCollection = client.db("tourismDb").collection("carts");
+  
+    
+// jwt  related api
+
+app.post('/jwt',async(req,res)=>{
+  const user=req.body;
+  const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:"1h"})
+  res.send({token});
+
+})
+
+//middlewares
+const verifyToken=(req,res,next)=>{
+  console.log( 'inside verify token',req.headers.authorization);
+  if(!req.headers.authorization){
+   return res.status(401).send({message:"fobidden access"})
+  }
+
+  const token=req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+if(err){
+  return res.status(401).send({message:"fobidden access"})
+
+}
+req.decoded=decoded
+ next();
+  }
+
+)
+
+ 
+
+}
+
+
+// user related api
+
+app.get('/users',verifyToken,async(req,res)=>{
+
+  const result=await userCollection.find().toArray();
+  res.send(result);
+})
+
+
+
+
+app.post('/users',async(req,res)=>{
+  const user=req.body; 
+  const query={email:user.email}
+  const existingUser=await userCollection.findOne(query)
+  if(existingUser){
+    return res.send({message:"user already exists",insertedId:null})
+
+  }
+  const result= await userCollection.insertOne(user);
+  res.send(result);
+})
+
+
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id=req.params.id;
+  const filter={_id:new ObjectId(id)}
+  const updatedDoc={
+    $set:{
+      role:'admin'
+    }
+  }
+  const result=await userCollection.updateOne(filter,updatedDoc);
+  res.send(result);
+})
+
+app.delete('/users/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)}
+   const result=await userCollection.deleteOne(query);
+   res.send(result)
+})
+
+
+
+
+
+//tourist realted api
+app.get('/tourist',async (req,res)=>{
+  const result = await spotCollection.find().toArray();
+  res.send(result);
+}) 
+
+// carts collection 
+
+app.get('/carts',async(req,res)=>{ 
+  const email=req.query.email;
+  const query={email: email};
+  const result=await cartCollection.find(query).toArray();
+  res.send(result);
+});
+
+app.delete('/carts/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)}
+  const result=await cartCollection.deleteOne(query);
+  res.send(result)
+})
+
+
+app.post('/carts',async(req,res)=>{
+  const cartItem=req.body;
+  const result=await cartCollection.insertOne(cartItem)
+  res.send(result);
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
